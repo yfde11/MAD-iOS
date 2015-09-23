6bbff1914836d3ebd012f08bb2d8cee7b48cd802
MichaelXiao <y19938222@gmail.com>
2015-09-23 22:00:48 +0800

    no message

---
+++ mad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mad
+//
+//  Created by MichaelXiao on 2015/9/23.
+//  Copyright © 2015年 MichaelXiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
